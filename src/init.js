598a76e762ac2af437691899c89acc823bbefa12
Nixeff <theodore.folkesson@gmail.com>
2022-11-11 16:14:36 +0100

    Victory

--- src/init.js
+++ src/init.js
@@ -266,7 +266,7 @@ function gameLoop() {
             }
             if(enemy.alive == false){
                 dialogueBox.onFinish = () => {
-                    gameScene = "PrisonCell";
+                    gameScene = "next";
                 }
                 dialogueBox.startDialogue([
                     {character:"",text:"(They died)"},
@@ -442,6 +442,9 @@ function gameLoop() {
                     buttons = [];
                 }
             }
+            break;
+            case "next":
+                alert("To be countinued???");
             break;
     }
 
