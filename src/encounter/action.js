697b54a6fb5d30ac5ee4f0c86c12059ea57e40f6
Nixeff <theodore.folkesson@gmail.com>
2022-11-13 21:33:57 +0100

    lol

--- src/encounter/action.js
+++ src/encounter/action.js
@@ -2,19 +2,12 @@
     array   | dice      -- Lista på alla tärningar som ska rullas om
 */
 function reRoll(dice,frameCounter,framesPerNumber,animationLength,frame,reRolling){
-    frameCounter++;
-        if(frameCounter>=framesPerNumber){
-            frame++
-            for(i = dice.length-1;i>=0;--i){
-                dice[i].roll();
-            }
-            if(frame>=animationLength){
-                frame = 0;
-                frameCounter = 0;
-                reRolling = false;
-            }
-        }
+    for(i = dice.length-1;i>=0;--i){
+        dice[i].roll();
+    }
+
 }
+
 
 /* enemyAttack
     array   | dice      -- Lista på alla tärningar som ska rullas om
